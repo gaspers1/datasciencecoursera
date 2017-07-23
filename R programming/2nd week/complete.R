89f4175206839d4df439b2ad7b4ceb9de6f12843
gaspers1 <gasper.stanovnik@gmail.com>
2017-07-23 12:19:40 +0200

    2nd programming assignment in 2nd week completed

---
+++ R programming/2nd week/complete.R
@@ -0,0 +1,39 @@
+
+
+
+complete <- function(directory = "specdata", id = 1:332) {
+
+    dirform <- paste(getwd(), "/", directory, "/", sep = "")
+    
+    for (i in dirform) {
+        files <- list.files(i)
+    }
+    
+    filepaths <- paste(dirform, files, sep="")
+    
+    comp <- c()
+    
+    for (i in id) {
+        
+        file <- read.csv(filepaths[i], header=T, sep=",")
+        
+        totalfile <- subset(file, file$nitrate >= 0 & file$sulfate >= 0)
+        
+        comp <- rbind(comp, c(i, length(totalfile$ID)))
+        
+    }
+    
+    colnames(comp) <- c("ID", "nobs")
+    
+    return(comp)
+    
+}
+
+
+
+
+complete("specdata", c(2, 4, 8, 10, 12))
+
+complete("specdata", 30:25)
+
+complete("specdata", 3)
