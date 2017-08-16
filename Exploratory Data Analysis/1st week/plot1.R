f3f167a3039791c08abcca4aa727c22de9361ddb
gaspers1 <gasper.stanovnik@gmail.com>
2017-08-16 10:13:14 +0200

    Apply all changes from forked repository

--- Exploratory Data Analysis/1st week/plot1.R
+++ Exploratory Data Analysis/1st week/plot1.R
@@ -1,30 +1,35 @@
 
 # estimate memory usage
 
-x <- 2075259
-y <- 9
+print(paste(round(2075259 * 9 * 8 / 1000000, 1), "MB"))
 
-mem_usage <- x * y * 8 / 1000000000
+print(object.size(df), units = "auto", standard = "SI") 
+print(object.size(df), units = "auto", standard = "IEC")
 
 # Upload, unzip and prepare dataset 
 
-setwd("./Development/R/datasciencecoursera/Exploratory Data Analysis/1st week")
+setwd("./Development/R/Exploratory Data Analysis")
 
 con <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 
 download.file(con, "power_consumption.zip")
-
 unzip("power_consumption.zip")
-
-if (file.exists("power_consumption.zip")) file.remove("power_consumption.zip")
-
-dir()
 
 df <- read.csv2("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
 
-df <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+if (file.exists("power_consumption.zip")) file.remove("power_consumption.zip")
+if (file.exists("household_power_consumption.txt")) file.remove("household_power_consumption.txt")
 
-df$Global_active_power <- as.numeric(df$Global_active_power)
+# Prepare data (subset and set numeric variables)
+
+Sys.setlocale("LC_TIME", "English") # Set local time to English for current session
+
+df <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+df$DTime <- strptime(paste(df$Date, df$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+for (i in 3:9) {
+    df[,i] <- as.numeric(df[,i]) # Set all other variables as numeric
+}
 
 # Plot 1
 
